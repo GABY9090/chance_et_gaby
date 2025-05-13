1404f1e616713a994c1824fc1203b5204e0a6d4f
butarechanceedwige@gmail.com <butarechanceedwige@gmail.com>
2025-05-13 21:46:19 +0200

    modif footer.php

--- import_produits.php
+++ import_produits.php
@@ -23,3 +23,28 @@ foreach ($produits as $p) {
 
 echo "✅ Importation terminée !";
 ?>
+<?php
+require_once 'config/database.php';
+require_once 'models/Produit.php';
+
+$json = file_get_contents('produits_fictifs.json');
+$produits = json_decode($json, true);
+
+// ID vendeur par défaut (ex. : 1)
+$id_vendeur = 1;
+
+foreach ($produits as $p) {
+    $produit = new Produit(
+        $p['nom'],
+        $p['prix'],
+        $p['image'],
+        $p['description'],
+        $p['categorie'],
+        $id_vendeur
+    );
+
+    $produit->ajouter($pdo);
+}
+
+echo "✅ Importation terminée !";
+?>
